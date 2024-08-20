b6542df67ca49b1fec9d653bb44766806903e733
kietndt <kietndt@TCIS-SSC-37128D>
2024-08-20 16:18:17 +0700

    [kietndt-20/08/2024] load data vehicle type

--- DATN.PARKING.UIUX/frmQueryReport.cs
+++ DATN.PARKING.UIUX/frmQueryReport.cs
@@ -1,20 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using DATN.PARKING.SERVICE.InterfaceMethod;
 
 namespace DATN.PARKING.UIUX
 {
     public partial class frmQueryReport : Form
     {
+        private readonly IServiceParking _service;
         public frmQueryReport()
         {
             InitializeComponent();
+        }
+        public frmQueryReport(IServiceParking service)
+        {
+            _service = service;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dtFromDate.DateTime >= dtToDate.DateTime)
+                {
+                    MessageBox.Show("Khoảng thời gian không hợp lệ. Vui lòng thử lại !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void lookVehicleType_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmQueryReport_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lookVehicleType.Properties.DataSource = _service.GetAllVehicleType();
+            }
+
+            catch(Exception ex)
+                {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
