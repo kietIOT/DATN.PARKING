d54fc98ab17ae09a51d5b6003a9880aa21c045da
kietndt <kietndt@TCIS-SSC-37128D>
2024-10-01 17:29:07 +0700

    [[kietcommuit]

---
+++ Auto_parking/Service/RegonizePlateLicense.cs
@@ -0,0 +1,399 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using tesseract;
+using System.IO.Ports;
+using System.IO;
+using Emgu.CV.CvEnum;
+
+namespace Auto_parking.Service
+{
+    public class RegonizePlateLicense
+    {
+        #region định nghĩa
+        List<Image<Bgr, byte>> PlateImagesList = new List<Image<Bgr, byte>>();
+        Image Plate_Draw;
+        List<string> PlateTextList = new List<string>();
+        List<Rectangle> listRect = new List<Rectangle>();
+        PictureBox[] box = new PictureBox[12];
+
+        public TesseractProcessor full_tesseract = null;
+        public TesseractProcessor ch_tesseract = null;
+        public TesseractProcessor num_tesseract = null;
+        private string m_path = Application.StartupPath + @"\data\";
+        private List<string> lstimages = new List<string>();
+        private const string m_lang = "eng";
+        #endregion
+
+
+        public void RegonizeInit()
+        {
+            full_tesseract = new TesseractProcessor();
+            bool succeed = full_tesseract.Init(m_path, m_lang, 3);
+            if (!succeed)
+            {
+                MessageBox.Show("Tesseract initialization failed. The application will exit.");
+                Application.Exit();
+            }
+            full_tesseract.SetVariable("tessedit_char_whitelist", "ABCDEFHKLMNPRSTVXY1234567890").ToString();
+
+            ch_tesseract = new TesseractProcessor();
+            succeed = ch_tesseract.Init(m_path, m_lang, 3);
+            if (!succeed)
+            {
+                MessageBox.Show("Tesseract initialization failed. The application will exit.");
+                Application.Exit();
+            }
+            ch_tesseract.SetVariable("tessedit_char_whitelist", "ABCDEFHKLMNPRSTUVXY").ToString();
+
+            num_tesseract = new TesseractProcessor();
+            succeed = num_tesseract.Init(m_path, m_lang, 3);
+            if (!succeed)
+            {
+                MessageBox.Show("Tesseract initialization failed. The application will exit.");
+                Application.Exit();
+            }
+            num_tesseract.SetVariable("tessedit_char_whitelist", "1234567890").ToString();
+
+
+            m_path = System.Environment.CurrentDirectory + "\\";
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < box.Length; i++)
+            {
+                box[i] = new PictureBox();
+            }
+        }
+
+        private void Reconize(string link, out Image hinhbienso, out string bienso, out string bienso_text)
+        {
+            for (int i = 0; i < box.Length; i++)
+            {
+                //this.Controls.Remove(box[i]);
+            }
+
+            hinhbienso = null;
+            bienso = "";
+            bienso_text = "";
+            ProcessImage(link);
+            if (PlateImagesList.Count != 0)
+            {
+                Image<Bgr, byte> src = new Image<Bgr, byte>(PlateImagesList[0].ToBitmap());
+                Bitmap grayframe;
+                FindContours con = new FindContours();
+                Bitmap color;
+                int c = con.IdentifyContours(src.ToBitmap(), 50, false, out grayframe, out color, out listRect);  // find contour
+                //int z = con.count;
+               // pictureBox_BiensoVAO.Image = color;
+               // IF.pictureBox1.Image = color;
+                hinhbienso = Plate_Draw;
+                //pictureBox_BiensoRA.Image = grayframe;
+               // IF.pictureBox3.Image = grayframe;
+                //textBox2.Text = c.ToString();
+                Image<Gray, byte> dst = new Image<Gray, byte>(grayframe);
+                //dst = dst.Dilate(2);
+                //dst = dst.Erode(3);
+                grayframe = dst.ToBitmap();
+                //pictureBox2.Image = grayframe.Clone(listRect[2], grayframe.PixelFormat);
+                string zz = "";
+
+                // lọc và sắp xếp số
+                List<Bitmap> bmp = new List<Bitmap>();
+                List<int> erode = new List<int>();
+                List<Rectangle> up = new List<Rectangle>();
+                List<Rectangle> dow = new List<Rectangle>();
+                int up_y = 0, dow_y = 0;
+                bool flag_up = false;
+
+                int di = 0;
+
+                if (listRect == null) return;
+
+                for (int i = 0; i < listRect.Count; i++)
+                {
+                    Bitmap ch = grayframe.Clone(listRect[i], grayframe.PixelFormat);
+                    int cou = 0;
+                    full_tesseract.Clear();
+                    full_tesseract.ClearAdaptiveClassifier();
+                    string temp = full_tesseract.Apply(ch);
+                    while (temp.Length > 3)
+                    {
+                        Image<Gray, byte> temp2 = new Image<Gray, byte>(ch);
+                        temp2 = temp2.Erode(2);
+                        ch = temp2.ToBitmap();
+                        full_tesseract.Clear();
+                        full_tesseract.ClearAdaptiveClassifier();
+                        temp = full_tesseract.Apply(ch);
+                        cou++;
+                        if (cou > 10)
+                        {
+                            listRect.RemoveAt(i);
+                            i--;
+                            di = 0;
+                            break;
+                        }
+                        di = cou;
+                    }
+                }
+
+                for (int i = 0; i < listRect.Count; i++)
+                {
+                    for (int j = i; j < listRect.Count; j++)
+                    {
+                        if (listRect[i].Y > listRect[j].Y + 100)
+                        {
+                            flag_up = true;
+                            up_y = listRect[j].Y;
+                            dow_y = listRect[i].Y;
+                            break;
+                        }
+                        else if (listRect[j].Y > listRect[i].Y + 100)
+                        {
+                            flag_up = true;
+                            up_y = listRect[i].Y;
+                            dow_y = listRect[j].Y;
+                            break;
+                        }
+                        if (flag_up == true) break;
+                    }
+                }
+
+                for (int i = 0; i < listRect.Count; i++)
+                {
+                    if (listRect[i].Y < up_y + 50 && listRect[i].Y > up_y - 50)
+                    {
+                        up.Add(listRect[i]);
+                    }
+                    else if (listRect[i].Y < dow_y + 50 && listRect[i].Y > dow_y - 50)
+                    {
+                        dow.Add(listRect[i]);
+                    }
+                }
+
+                if (flag_up == false) dow = listRect;
+
+                for (int i = 0; i < up.Count; i++)
+                {
+                    for (int j = i; j < up.Count; j++)
+                    {
+                        if (up[i].X > up[j].X)
+                        {
+                            Rectangle w = up[i];
+                            up[i] = up[j];
+                            up[j] = w;
+                        }
+                    }
+                }
+                for (int i = 0; i < dow.Count; i++)
+                {
+                    for (int j = i; j < dow.Count; j++)
+                    {
+                        if (dow[i].X > dow[j].X)
+                        {
+                            Rectangle w = dow[i];
+                            dow[i] = dow[j];
+                            dow[j] = w;
+                        }
+                    }
+                }
+
+                int x = 12;
+                int c_x = 0;
+
+                for (int i = 0; i < up.Count; i++)
+                {
+                    Bitmap ch = grayframe.Clone(up[i], grayframe.PixelFormat);
+                    Bitmap o = ch;
+                    //ch = con.Erodetion(ch);
+                    string temp;
+                    if (i < 2)
+                    {
+                        temp = Ocr(ch, false, true); // nhan dien so
+                    }
+                    else
+                    {
+                        temp = Ocr(ch, false, false);// nhan dien chu
+                    }
+
+                    zz += temp;
+                    box[i].Location = new Point(x + i * 50, 290);
+                    box[i].Size = new Size(50, 100);
+                    box[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                    box[i].Image = ch;
+                    box[i].Update();
+                    //this.Controls.Add(box[i]);
+                   // IF.Controls.Add(box[i]);
+                    c_x++;
+                }
+                zz += "\r\n";
+                for (int i = 0; i < dow.Count; i++)
+                {
+                    Bitmap ch = grayframe.Clone(dow[i], grayframe.PixelFormat);
+                    //ch = con.Erodetion(ch);
+                    string temp = Ocr(ch, false, true); // nhan dien so
+                    zz += temp;
+                    box[i + c_x].Location = new Point(x + i * 50, 390);
+                    box[i + c_x].Size = new Size(50, 100);
+                    box[i + c_x].SizeMode = PictureBoxSizeMode.StretchImage;
+                    box[i + c_x].Image = ch;
+                    box[i + c_x].Update();
+                    //this.Controls.Add(box[i + c_x]);
+                   // IF.Controls.Add(box[i + c_x]);
+                }
+                bienso = zz.Replace("\n", "");
+                bienso = bienso.Replace("\r", "");
+               // IF.textBox6.Text = zz;
+                bienso_text = zz;
+
+            }
+        }
+        private string Ocr(Bitmap image_s, bool isFull, bool isNum = false)
+        {
+            string temp = "";
+            Image<Gray, byte> src = new Image<Gray, byte>(image_s);
+            double ratio = 1;
+            while (true)
+            {
+                ratio = (double)CvInvoke.cvCountNonZero(src) / (src.Width * src.Height);
+                if (ratio > 0.5) break;
+                src = src.Dilate(2);
+            }
+            Bitmap image = src.ToBitmap();
+
+            TesseractProcessor ocr;
+            if (isFull)
+                ocr = full_tesseract;
+            else if (isNum)
+                ocr = num_tesseract;
+            else
+                ocr = ch_tesseract;
+
+            int cou = 0;
+            ocr.Clear();
+            ocr.ClearAdaptiveClassifier();
+            temp = ocr.Apply(image);
+            while (temp.Length > 3)
+            {
+                Image<Gray, byte> temp2 = new Image<Gray, byte>(image);
+                temp2 = temp2.Erode(2);
+                image = temp2.ToBitmap();
+                ocr.Clear();
+                ocr.ClearAdaptiveClassifier();
+                temp = ocr.Apply(image);
+                cou++;
+                if (cou > 10)
+                {
+                    temp = "";
+                    break;
+                }
+            }
+            return temp;
+
+        }
+        public void ProcessImage(string urlImage)
+        {
+            PlateImagesList.Clear();
+            PlateTextList.Clear();
+            FileStream fs = new FileStream(urlImage, FileMode.Open, FileAccess.Read);
+            Image img = Image.FromStream(fs);
+            Bitmap image = new Bitmap(img);
+            //pictureBox2.Image = image;
+            //IF.pictureBox2.Image = image;
+            fs.Close();
+
+            FindLicensePlate4(image, out Plate_Draw);
+
+        }
+        public void FindLicensePlate4(Bitmap image, out Image plateDraw)
+        {
+            plateDraw = null;
+            Image<Bgr, byte> frame;
+            bool isface = false;
+            Bitmap src;
+            //pictureBox2.Image = new Image<Gray, byte>(image).ToBitmap();
+            Image dst = image;
+            HaarCascade haar = new HaarCascade(Application.StartupPath + "\\output-hv-33-x25.xml");
+            for (float i = 0; i <= 20; i = i + 3)
+            {
+                for (float s = -1; s <= 1 && s + i != 1; s += 2)
+                {
+                    src = RotateImage(dst, i * s);
+                    PlateImagesList.Clear();
+                    frame = new Image<Bgr, byte>(src);
+                    using (Image<Gray, byte> grayframe = new Image<Gray, byte>(src))
+                    {
+                        var faces =
+                       grayframe.DetectHaarCascade(haar, 1.1, 8, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(0, 0))[0];
+                        foreach (var face in faces)
+                        {
+                            Image<Bgr, byte> tmp = frame.Copy();
+                            tmp.ROI = face.rect;
+
+                            frame.Draw(face.rect, new Bgr(Color.Blue), 2);
+
+                            PlateImagesList.Add(tmp);
+
+                            isface = true;
+                        }
+                        if (isface)
+                        {
+                            Image<Bgr, byte> showimg = frame.Clone();
+                            plateDraw = (Image)showimg.ToBitmap();
+                            //showimg = frame.Resize(imageBox1.Width, imageBox1.Height, 0);
+                            //pictureBox1.Image = showimg.ToBitmap();
+                            //IF.pictureBox2.Image = showimg.ToBitmap();
+                            if (PlateImagesList.Count > 1)
+                            {
+                                for (int k = 1; k < PlateImagesList.Count; k++)
+                                {
+                                    if (PlateImagesList[0].Width < PlateImagesList[k].Width)
+                                    {
+                                        PlateImagesList[0] = PlateImagesList[k];
+                                    }
+                                }
+                            }
+                            PlateImagesList[0] = PlateImagesList[0].Resize(400, 400, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+                            return;
+                        }
+
+
+                    }
+                }
+            }
+
+
+        }
+        public static Bitmap RotateImage(Image image, float angle)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image");
+
+            PointF offset = new PointF((float)image.Width / 2, (float)image.Height / 2);
+
+            //create a new empty bitmap to hold rotated image
+            Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //make a graphics object from the empty bitmap
+            Graphics g = Graphics.FromImage(rotatedBmp);
+
+            //Put the rotation point in the center of the image
+            g.TranslateTransform(offset.X, offset.Y);
+
+            //rotate the image
+            g.RotateTransform(angle);
+
+            //move the image back
+            g.TranslateTransform(-offset.X, -offset.Y);
+
+            //draw passed in image onto graphics object
+            g.DrawImage(image, new PointF(0, 0));
+
+            return rotatedBmp;
+        }
+    }
+}
